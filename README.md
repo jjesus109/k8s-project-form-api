7df2909d228b0f9a18101f34b45409cddb1d5d92
jjesus109 <jesjalher@gmail.com>
2022-06-09 23:36:29 -0500

    Add CICD on README

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ To run it it is necessary to set the following env vars:
     DB_HOST
     DB_PORT
     DB_NAME
+
+## CI/CD
+
+docker build -t kubernetes-course:form-api-[version] -f Dockerfile .
+docker tag  kubernetes-course:form-api-[version] jesu11s/kubernetes-course:form-api-[version]
--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
 from typing import List
 
-from fastapi import FastAPI, Depends
 from app.db import init_db, get_session
 from app.models import Form, FormBase
+
+from fastapi import FastAPI, Depends
 from sqlmodel import Session, select
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 app = FastAPI()
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
